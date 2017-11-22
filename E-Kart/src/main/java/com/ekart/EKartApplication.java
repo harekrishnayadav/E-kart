230136520c6d4f796555aa9412ee87c4e55f8259
sujeet <ka@gmai98l.com>
2017-11-22 14:38:07 +0530

    2nd project start

---
+++ E-Kart/src/main/java/com/ekart/EKartApplication.java
@@ -0,0 +1,12 @@
+package com.ekart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EKartApplication.class, args);
+	}
+}
