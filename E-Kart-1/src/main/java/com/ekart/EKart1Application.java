e1782e1155c1b3f90cb7dc605e70ac72f16d4613
sujeet <harekrishna.bhumca13@gmai98l.com>
2017-11-22 15:00:16 +0530

    update

---
+++ E-Kart-1/src/main/java/com/ekart/EKart1Application.java
@@ -0,0 +1,12 @@
+package com.ekart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EKart1Application.class, args);
+	}
+}
